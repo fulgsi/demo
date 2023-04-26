c6fb6072327fb97f3e0157481801b7dc6423e252
fuls <809475285@qq.com>
2023-04-26 10:39:30 +0800

    third commit

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,11 +3,15 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootTest
 class DemoApplicationTests {
 
     @Test
     void contextLoads() {
+        Map<String, Object> map = new HashMap<>();
     }
 
 }
